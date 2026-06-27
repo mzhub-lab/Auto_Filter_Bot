3402e141e10eb61e5f5c533296846272a98b49c6
mzhub-lab <yogedrasama@gmail.com>
2026-06-27 20:47:53 +0530

    Update AUTH_REQ_CHANNELS environment variable

--- info.py
+++ info.py
@@ -55,7 +55,7 @@ reqst_channel = environ.get('REQST_CHANNEL_ID', '-1004434857614')  # Request cha
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/drbotsupport')  # Support group link (make sure bot is admin)
 
 # FORCE_SUB 
-auth_req_channels = environ.get("AUTH_REQ_CHANNELS", "-1003948498853")# requst to join Channel for force sub (make sure bot is admin) only for bot ADMINS  
+auth_req_channels = environ.get("AUTH_REQ_CHANNELS", "-1003998775841")# requst to join Channel for force sub (make sure bot is admin) only for bot ADMINS  
 auth_channels     = environ.get("AUTH_CHANNELS", "-1003948498853")# Channels for force sub (make sure bot is admin)
 
 # ============================
