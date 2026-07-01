ba802baca20b08147d65cbe9a2db37941f0e1c15
mzhub-lab <yogedrasama@gmail.com>
2026-07-01 18:10:24 +0530

    Update info.py

--- info.py
+++ info.py
@@ -62,7 +62,7 @@ auth_channels     = environ.get("AUTH_CHANNELS", "-1003948498853")# Channels for
 # Payment Configuration
 # ============================
 QR_CODE = environ.get('QR_CODE', 'https://files.catbox.moe/uycn3p.jpg')    # QR code image for payments
-OWNER_UPI_ID = environ.get('ybawaskar@slc', 'ɴᴏ ᴀᴠᴀɪʟᴀʙʟᴇ ʀɪɢʜᴛ ɴᴏᴡ')    # Owner UPI ID for payments
+OWNER_UPI_ID = environ.get('OWNER_UPI_ID', 'ybawaskar@slc')    # Owner UPI ID for payments
 
 STAR_PREMIUM_PLANS = {
     10: "15day",
